c49e41ba8e4d6a2ee0b47e514db8957668242a5b
Donald J. Santos <DonaldJSantos@Gmail.com>
2014-09-10 01:22:54 -0700

    Lab1
    Lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            char middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCM;
+
+            firstName = "Donald";
+            middleInitial = 'J';
+            lastName = "Santos";
+
+            fullName = firstName + " " + middleInitial + ". " + lastName;
+
+            age = 35;
+            heightFeet = 6;
+            heightInches = 1.25;
+            totalHeightCM = 2.54 * ((heightFeet * 12) + heightInches);
+
+            isCitizen = true;
+            canVote = isCitizen && (age >= 18);
+
+            Console.WriteLine(fullName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab1/Lab1_Extras/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab1_Extras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First Name:             ");
+            string firstName = Console.ReadLine();
+            Console.Write("Middle Initial:         ");
+            string middleInitial = Console.ReadLine();
+            Console.Write("Last Name:              ");
+            string lastName = Console.ReadLine();
+            string fullName = firstName + " " + middleInitial + ". " + lastName;
+
+            Console.Write("Age:                    ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Height In Feet: (4/5/6) ");
+            int heightFeet = int.Parse(Console.ReadLine());
+            Console.Write("Extra Inches:           ");
+            double heightInches = double.Parse(Console.ReadLine());
+            double totalHeightCM = 2.54 * ((heightFeet * 12) + heightInches);
+
+            Console.Write("Is Citizen: (y/n)       ");
+            bool isCitizen = Console.ReadLine().ToLower().StartsWith("y");
+            bool canVote = isCitizen && (age >= 18);
+
+            Console.WriteLine();
+            Console.WriteLine("Full Name : " + fullName);
+            Console.WriteLine("Height CM : " + totalHeightCM);
+            Console.WriteLine("Can Vote  : " + canVote);
+            Console.ReadLine();
+        }
+    }
+}
