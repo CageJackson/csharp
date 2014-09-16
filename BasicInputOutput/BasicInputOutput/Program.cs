20b6ceafdd31193626cdf7d69703af5c29dc62f9
Donald J. Santos <DonaldJSantos@Gmail.com>
2014-09-16 15:29:08 -0700

    Basic Input Output
    Basic Input Output

---
+++ BasicInputOutput/BasicInputOutput/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BasicInputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringInput;
+            int integerInput;
+            bool booleanInput;
+
+            Console.Write("Enter A String        : ");
+            stringInput = Console.ReadLine();
+
+            Console.Write("Enter An Integer      : ");
+            integerInput = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter A Boolean (Y/N) : ");
+            booleanInput = Console.ReadLine().ToUpper().StartsWith("Y");
+
+            Console.WriteLine();
+            Console.WriteLine(stringInput);
+            Console.WriteLine(integerInput);
+            Console.WriteLine(booleanInput);
+
+            Console.WriteLine();
+            Console.WriteLine("Hit Enter To End Program...");
+            Console.ReadLine();
+        }
+    }
+}
