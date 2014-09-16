51cb10a9748ae28f07bfe9fef907f68fd9037cc0
Donald J. Santos <DonaldJSantos@Gmail.com>
2014-09-16 16:12:11 -0700

    Making Decisions
    Making Decisions

---
+++ MakingDecisions/MakingDecisions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MakingDecisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your Eye Color?    : ");
+            string eyeColor = Console.ReadLine().ToLower();
+
+            Console.Write("What is your Gender? (M/F) : ");
+            bool isMale = Console.ReadLine().ToUpper().StartsWith("M");
+
+            string name;
+
+            switch (eyeColor)
+            {
+                case "blue": name = isMale ? "Joe" : "Sally";  break;
+                case "brown": name = isMale ? "Dave" : "Jane"; break;
+                case "green": name = isMale ? "John" : "Phoebe"; break;
+                default: name = "Not Found"; break;
+            }
+
+
+            Console.WriteLine("Your Name Is " + name + ".");
+            Console.ReadLine();
+        }
+    }
+}
