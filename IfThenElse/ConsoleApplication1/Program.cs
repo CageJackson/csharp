ef3c62101a420041266e572bdaa646a78c564d9f
Donald J. Santos <DonaldJSantos@Gmail.com>
2014-09-16 16:23:35 -0700

    IfThenElse
    IfThenElse

---
+++ IfThenElse/ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 1000);
+
+            ///////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////
+
+            if (randomNumber > 500)
+            {
+                Console.WriteLine("High");
+            }
+
+            ///////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////
+
+            if (randomNumber > 500)
+            {
+                Console.WriteLine("High");
+            }
+            else
+            {
+                Console.WriteLine("Low");
+            }
+
+            ///////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////
+
+            if (randomNumber > 666)
+            {
+                Console.WriteLine("High");
+            }
+            else if (randomNumber > 333)
+            {
+                Console.WriteLine("Medium");
+            }
+            else
+            {
+                Console.WriteLine("Low");
+            }
+
+            ///////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////
+
+            Console.ReadLine();
+        }
+    }
+}
