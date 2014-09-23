405feb48e626c0b170bbc22f951824d2c1ed4224
Donald J. Santos <DonaldJSantos@Gmail.com>
2014-09-22 22:42:45 -0400

    Update
    Update

---
+++ ClassesAndObjects/Example4/Point.cs
@@ -0,0 +1,10 @@
+﻿namespace Example4
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+
+        public static Point Origin;
+    }
+}
---
+++ ClassesAndObjects/Example4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.X = 10;
+            p1.Y = 20;
+
+            Point p2 = new Point();
+            p2.X = 30;
+            p2.Y = 30;
+
+            Point.Origin = new Point();
+            Point.Origin.X = 0;
+            Point.Origin.Y = 0;
+        }
+    }
+}
