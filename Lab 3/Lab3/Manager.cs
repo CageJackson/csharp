e5ba2bb22bc193efb6fb7467481838b49c3876f9
Donald J. Santos <DonaldJSantos@Gmail.com>
2014-09-22 22:06:35 -0400

    Lab 3
    Lab 3

---
+++ Lab 3/Lab3/Manager.cs
@@ -0,0 +1,56 @@
+﻿namespace Lab3Hard_Solution
+{
+    public class Manager
+    {
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+
+        private double sumOfAllAges;
+        private int totalNumberOfPeople;
+
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+
+        public void UpdatePersonStatics(Person person)
+        {
+            updateAverageAge(person);
+            updateOldestAndYoungest(person);
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+
+        private void updateAverageAge(Person person)
+        {
+            sumOfAllAges += person.Age;
+            totalNumberOfPeople++;
+
+            if (person.IsMarried())
+            {
+                sumOfAllAges += person.Spouse.Age;
+                totalNumberOfPeople++;
+            }
+
+            Person.AverageAge = sumOfAllAges / totalNumberOfPeople;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+
+        private void updateOldestAndYoungest(Person person)
+        {
+            if (Person.Oldest == null || person.Age > Person.Oldest.Age)
+            {
+                Person.Oldest = person;
+            }
+
+            if (Person.Youngest == null || person.Age < Person.Youngest.Age)
+            {
+                Person.Youngest = person;
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+    }
+}
---
+++ Lab 3/Lab3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lab3Hard_Solution
+{
+    class Program
+    {
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            manager.UpdatePersonStatics(createPerson("Person #1"));
+            manager.UpdatePersonStatics(createPerson("Person #2"));
+            manager.UpdatePersonStatics(createPerson("Person #3"));
+            Person.PrintStaticValues();
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+
+        private static Person createPerson(string promptPrefix)
+        {
+            Question.SetPromptPrefix(promptPrefix);
+
+            Person person = new Person();
+            person.FirstName = Question.AskForString("First Name");
+            person.LastName = Question.AskForString("Last Name");
+            person.Age = Question.AskForInteger("Age");
+
+            if (Question.AskForBoolean("Married?"))
+            {
+                person.Spouse = new Person();
+                person.Spouse.FirstName = Question.AskForString("Spouse Name");
+                person.Spouse.Age = Question.AskForInteger("Spouse Age");
+                person.Spouse.LastName = person.LastName;
+                person.Spouse.Spouse = person;
+            }
+
+            Console.WriteLine();
+
+            return person;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+    }
+}
