db824db3bd2ef8856ba59ec385e024b6ca94fd32
Donald J. Santos <DonaldJSantos@Gmail.com>
2014-09-22 22:21:46 -0400

    Updates
    Updates

---
+++ ClassesAndObjects/Example3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var record = new Record();
+            record.OwnerName = "Donnie";
+            record.OwnerID = 123;
+            record.IsActive = true;
+
+            Console.WriteLine(record.GetFormattedString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassesAndObjects/Example3/Record.cs
@@ -0,0 +1,16 @@
+﻿namespace Example3
+{
+    public class Record
+    {
+        public string OwnerName { get; set; }
+        public int OwnerID { get; set; }
+        public bool IsActive { get; set; }
+
+        public string GetFormattedString()
+        {
+            string idText = "[" + this.OwnerID + "]";
+            string isActiveText = IsActive ? "(active)" : "(inactive)";
+            return idText + " " + this.OwnerName + " " + isActiveText;
+        }
+    }
+}
